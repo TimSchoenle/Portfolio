3d5db58ae4dfffe05e3f2260006df6405538e7ca
Timmi6790 <13711365+Timmi6790@users.noreply.github.com>
2025-11-10 08:05:05 +0100

    fix: invalid public headers

--- next.config.ts
+++ next.config.ts
@@ -23,37 +23,8 @@ const nextConfig: NextConfig = {
     typedEnv: true,
   },
 
-  headers(): {
-    source: string
-    headers: { key: string; value: string }[]
-  }[] {
-    return [
-      {
-        headers: [
-          { key: 'X-DNS-Prefetch-Control', value: 'on' },
-          {
-            key: 'Strict-Transport-Security',
-            value: 'max-age=63072000; includeSubDomains; preload',
-          },
-          { key: 'X-Frame-Options', value: 'SAMEORIGIN' },
-          { key: 'X-Content-Type-Options', value: 'nosniff' },
-          { key: 'Referrer-Policy', value: 'origin-when-cross-origin' },
-          {
-            key: 'Permissions-Policy',
-            value: 'camera=(), microphone=(), geolocation=()',
-          },
-        ],
-        source: '/:path*',
-      },
-    ]
-  },
-
   images: {
     formats: ['image/avif', 'image/webp'],
-    remotePatterns: [
-      { hostname: 'github.com', protocol: 'https' },
-      { hostname: 'avatars.githubusercontent.com', protocol: 'https' },
-    ],
   },
 
   output: 'standalone',
